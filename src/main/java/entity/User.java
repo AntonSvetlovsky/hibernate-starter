f47c7815516143367def09a222692b98baea33fe
Anton <antonsvetlovsky@gmail.com>
2022-10-03 09:31:49 +0300

    Add entity

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,35 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users", schema = "public")
+public class User {
+    @Id
+    private String username;
+
+    private String firstname;
+
+    private String lastname;
+
+    @Column(name = "birth_date")//if
+                                // configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+                                //was not configured
+    private LocalDate birthDate;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
---
+++ src/main/java/org/example/HibernateRunner.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import entity.Role;
+import entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
+
+public class HibernateRunner {
+    public static void main(String[] args) {
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        configuration.addAnnotatedClass(User.class);
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 1))
+                    .age(20)
+                    .role(Role.ADMIN)
+                    .build();
+            session.save(user);
+            session.getTransaction().commit();
+        }
+    }
+}
